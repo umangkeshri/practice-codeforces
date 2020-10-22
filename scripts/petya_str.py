3371d14c0930465b5fa503479fb2ca227e2bdbaf
Umang Keshri <umang.keshri144@gmail.com>
2020-10-22 16:24:31 +0530

    112A solution

---
+++ scripts/petya_str.py
@@ -0,0 +1,11 @@
+# Problem Link: https://codeforces.com/problemset/problem/112/A
+
+s1 = input().lower()
+s2 = input().lower()
+
+if s1 == s2:
+    print(0)
+elif s1 < s2:
+    print(-1)
+else:
+    print(1)
